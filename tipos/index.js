aac52196d34b07bb643f40cda70ad880e8f92162
kauabarros-24 <martinsbarroskaua@gmail.com>
2024-06-27 14:47:42 -0300

    Função anônima e objetos em js

--- tipos/index.js
+++ tipos/index.js
@@ -1,5 +1,11 @@
 "use strict";
 setTimeout(function () {
     const sallary = 100;
-    console.l;
-});
+    console.log(sallary);
+}, 2000);
+function passCoordinates(coord) {
+    console.log("X coordenadas" + coord.x);
+    console.log("Y coordenadas" + coord.y);
+}
+const objCoord = { x: 398, y: 245 };
+passCoordinates(objCoord);
--- tipos/index.ts
+++ tipos/index.ts
@@ -1,4 +1,11 @@
-setTimeout(function() {
-    const sallary: number = 100
-    console.l
-})
+setTimeout(function() {const sallary: number = 100; console.log(sallary)
+},2000)
+
+function passCoordinates(coord: {x: number, y: number}) {
+    console.log("X coordenadas" + coord.x)
+    console.log("Y coordenadas" + coord.y)
+}
+
+const objCoord = {x: 398, y: 245}
+
+passCoordinates(objCoord)
