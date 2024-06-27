f6f0dcb5b0a310c26f3e8e43be0cc419bd982fea
kauabarros-24 <martinsbarroskaua@gmail.com>
2024-06-27 13:56:55 -0300

    Any

--- tipos/index.js
+++ tipos/index.js
@@ -1,1 +1,14 @@
 "use strict";
+//
+let numbers = [1, 2, 3];
+numbers.push(10);
+for (let i = 0; i < numbers.length; i++) {
+    console.log(numbers[i]);
+}
+const num = [1, 2, 4];
+console.log(num);
+//ANY
+const todos = [1, 2, 3, "Kaua", true];
+for (let i = 0; i < todos.length; i++) {
+    console.log(todos[i]);
+}
