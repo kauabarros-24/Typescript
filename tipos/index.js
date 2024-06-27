cffd5d3a426223f45e05cd3f775646499794a351
kauabarros-24 <martinsbarroskaua@gmail.com>
2024-06-27 15:00:16 -0300

    Balanço

--- tipos/index.js
+++ tipos/index.js
@@ -9,3 +9,28 @@ function passCoordinates(coord) {
 }
 const objCoord = { x: 398, y: 245 };
 passCoordinates(objCoord);
+//Proposições opcionais
+function showNumbers(a, b, c) {
+    console.log("A" + a);
+    console.log("B" + b);
+    console.log("C" + c);
+}
+//Validando argumento opcional
+const ame = "Kaua";
+const lastame = "Barros";
+function validacao(firstname, lastName) {
+    if (lastName !== undefined) {
+        console.log("lastName foi validado");
+    }
+    else {
+        console.log("lastName não foi validado");
+    }
+}
+validacao(ame, lastame);
+validacao(ame);
+//Union Type
+function showBalance(balance) {
+    console.log("O balanço é de " + balance);
+}
+showBalance(500);
+showBalance("100");
--- tipos/index.ts
+++ tipos/index.ts
@@ -9,3 +9,33 @@ function passCoordinates(coord: {x: number, y: number}) {
 const objCoord = {x: 398, y: 245}
 
 passCoordinates(objCoord)
+
+//Proposições opcionais
+function showNumbers(a: number, b:number, c:number) {
+    console.log("A" + a)
+    console.log("B" + b)
+    console.log("C" + c)
+}
+
+//Validando argumento opcional
+const ame: string = "Kaua"
+const lastame: string = "Barros"
+
+function validacao(firstname: string, lastName: string) {
+    if (lastName !== undefined) {
+        console.log("lastName foi validado")
+    } else {
+        console.log("lastName não foi validado")
+    }
+}
+
+validacao(ame, lastame)
+validacao(ame)
+
+//Union Type
+function showBalance(balance: string  | number ) {
+    console.log("O balanço é de " + balance)
+}
+
+showBalance(500)
+showBalance("100")
