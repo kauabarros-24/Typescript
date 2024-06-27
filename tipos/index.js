d8530cb5473cedaf99711b7f6da82ed022874b8d
kauabarros-24 <martinsbarroskaua@gmail.com>
2024-06-27 14:32:28 -0300

    Funções anônimas

--- tipos/index.js
+++ tipos/index.js
@@ -1,14 +1,5 @@
 "use strict";
-//
-let numbers = [1, 2, 3];
-numbers.push(10);
-for (let i = 0; i < numbers.length; i++) {
-    console.log(numbers[i]);
-}
-const num = [1, 2, 4];
-console.log(num);
-//ANY
-const todos = [1, 2, 3, "Kaua", true];
-for (let i = 0; i < todos.length; i++) {
-    console.log(todos[i]);
-}
+setTimeout(function () {
+    const sallary = 100;
+    console.l;
+});
--- tipos/index.ts
+++ tipos/index.ts
@@ -1,18 +1,4 @@
-//
-
-let numbers: number[] = [1,2,3]
-
-numbers.push(10)
-for (let i = 0; i < numbers.length; i++) {
-    console.log(numbers[i]);
-    
-}
-const num: Array<number> = [1,2,4]
-console.log(num)
-
-//ANY
-const todos: Array<any> = [1, 2, 3, "Kaua", true]
-for (let i = 0; i < todos.length; i++) {
-    console.log(todos[i]);
-    
-}
+setTimeout(function() {
+    const sallary: number = 100
+    console.l
+})
